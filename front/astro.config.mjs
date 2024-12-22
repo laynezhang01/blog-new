af556ff287cd9755068959bf79b829a49996e085
laynezhang <meetzhang@hotmail.com>
2024-12-22 16:22:20 +0800

    init

---
+++ front/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        mdx()
+    ],
+    adapter: netlify(),
+    output: 'server',
+});
